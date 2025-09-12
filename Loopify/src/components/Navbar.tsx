e668615b3299d3ef0b9a769680ac7ea29bbf9ed5
DiceX <149762497+Thinnapop@users.noreply.github.com>
2025-09-12 15:27:35 +0700

    Update Navbar.tsx

--- Loopify/src/components/Navbar.tsx
+++ Loopify/src/components/Navbar.tsx
@@ -33,7 +33,7 @@ const combinedStyles = `
     top: 120%;
     right: 0;
     z-index: 10;
-    width: 400px;
+    width: 275px;
     background-color: #282828;
     border-radius: 8px;
     box-shadow: 0 4px 12px rgba(0,0,0,0.5);
@@ -77,7 +77,7 @@ interface NavbarProps {
 }
 const Navbar: React.FC<NavbarProps> = ({ onLoginClick, onRegisterClick }) => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const notifications = [ "New album from your favorite artist!", "Your weekly playlist is ready."];
+  const notifications = [ "New song from your favorite artist has release!", "Your weekly playlist is ready."];
 
   return (
     <>
@@ -115,7 +115,7 @@ const Navbar: React.FC<NavbarProps> = ({ onLoginClick, onRegisterClick }) => {
           
           <div className="notification-container">
             <button className="nav-icon-btn" onClick={() => setDropdownOpen(!isDropdownOpen)}>
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+              <svg width="16" height="24" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M12 22C13.1 22 14 21.1 14 20H10C10 21.1 10.9 22 12 22ZM18 16V11C18 7.93 16.37 5.36 13.5 4.68V4C13.5 3.17 12.83 2.5 12 2.5C11.17 2.5 10.5 3.17 10.5 4V4.68C7.64 5.36 6 7.92 6 11V16L4 18V19H20V18L18 16Z"/>
               </svg>
             </button>
